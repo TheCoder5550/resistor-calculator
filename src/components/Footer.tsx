2ac08faf7e86e249fea40bddbb0f1babed4adadb
TheCoder5550 <alfons.nilsson@outlook.com>
2024-01-05 20:07:16 +0100

    Fixed GitHub url in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <div style={{ marginTop: "5rem" }}>
       <span>Created by <a href="https://aaserver.net/">Alfons Nilsson</a></span>
       &nbsp;&#8226;&nbsp;
-      <span>Source code on <a href="#">GitHub</a></span>
+      <span>Source code on <a href="https://github.com/TheCoder5550/resistor-calculator">GitHub</a></span>
     </div>
   )
 }
